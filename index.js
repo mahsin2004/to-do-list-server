dbd88b129fe71ef8d21b803758445884f15ff9cf
mahsin <mahsin1912@gmail.com>
2023-12-21 16:06:11 +0600

    Server Ready

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello Client!");
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Brand Shop is Running at ${port}`);
+});
